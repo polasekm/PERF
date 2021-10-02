3606c4e55cc70d561d9514fc30a322547d48f530
Martin Polasek <polasek.martin@seznam.cz>
2021-10-02 02:00:11 +0200

    First

---
+++ perf.c
@@ -0,0 +1,63 @@
+/*
+ * perf.c
+ *
+ *  Created on: 13. 3. 2021
+ *      Author: polasekm
+ */
+//------------------------------------------------------------------------------
+#include "perf.h"
+
+#include "tim.h"
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+void perf_init(perf_t *perf)
+{
+  perf->cnt = 0;
+  perf->cnt_int = 0;
+
+  perf->time_start = htim5.Instance->CNT;
+
+  perf->time_diff = 0;
+  perf->time_min = 0;
+  perf->time_max = 0;
+  perf->time_avg = 0;
+
+  perf->time_min_int = 0;
+  perf->time_max_int = 0;
+}
+//------------------------------------------------------------------------------
+void perf_start(perf_t *perf)
+{
+  perf->time_start = htim5.Instance->CNT;
+  perf->cnt++;
+}
+//------------------------------------------------------------------------------
+void perf_end_start(perf_t *perf)
+{
+  perf_end(perf);
+  perf_start(perf);
+}
+//------------------------------------------------------------------------------
+void perf_end(perf_t *perf)
+{
+  perf->time_diff = htim5.Instance->CNT - perf->time_start;
+
+  if(perf->time_diff < perf->time_min) perf->time_min = perf->time_diff;
+  if(perf->time_diff > perf->time_max) perf->time_max = perf->time_diff;
+
+  perf->time_avg += (float)perf->time_diff * 0.001 - perf->time_avg * 0.001;
+}
+//------------------------------------------------------------------------------
+void perf_int_proc(perf_t *perf)
+{
+  perf->cnt_int = perf->cnt;
+  perf->cnt = 0;
+
+  perf->time_min_int = perf->time_min;
+  perf->time_min = perf->time_avg;
+
+  perf->time_max_int = perf->time_max;
+  perf->time_max = perf->time_avg;
+}
+//------------------------------------------------------------------------------
---
+++ perf.h
@@ -0,0 +1,59 @@
+/*
+ * perf.h
+ *
+ *  Created on: 13. 3. 2021
+ *      Author: polasekm
+ */
+/*-----------------------------------------------------------------------------*/
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef PERF_PERF_H_
+#define PERF_PERF_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdint.h>
+
+/* Exported types ------------------------------------------------------------*/
+/**
+ * @brief  Ring buffer structure definition
+ */
+typedef struct
+{
+  uint32_t cnt;
+  uint32_t cnt_int;
+
+  uint32_t time_start;
+
+  uint32_t time_diff;
+  uint32_t time_min;
+  uint32_t time_max;
+  float time_avg;
+
+  uint32_t time_min_int;
+  uint32_t time_max_int;
+
+} perf_t;
+
+/* Exported functions ------------------------------------------------------- */
+
+/* Defines -------------------------------------------------------------------*/
+// Functions -------------------------------------------------------------------
+
+void perf_init(perf_t *perf);
+
+void perf_start(perf_t *perf);
+void perf_end_start(perf_t *perf);
+void perf_end(perf_t *perf);
+
+void perf_int_proc(perf_t *perf);
+
+   //------------------------------------------------------------------------------
+#ifdef __cplusplus
+}
+#endif
+//------------------------------------------------------------------------------
+#endif /* PERF_PERF_H_ */

fc01018f4a6fe5322ef9539bc0a4a7d7b3315477
Martin Polasek <polasek.martin@seznam.cz>
2024-10-25 12:49:56 +0200

    v2

--- perf.c
+++ perf.c
@@ -13,51 +13,68 @@
 //------------------------------------------------------------------------------
 void perf_init(perf_t *perf)
 {
-  perf->cnt = 0;
-  perf->cnt_int = 0;
+  perf->time_last = 0;
 
-  perf->time_start = PERF_GET_TICK();//htim2.Instance->CNT;
-
-  perf->time_diff = 0;
-  perf->time_min = 0;
+  perf->time_min = (uint32_t)-1;
   perf->time_max = 0;
-  perf->time_avg = 0;
 
-  perf->time_min_int = 0;
-  perf->time_max_int = 0;
+  perf->time_tot = 0;
+  perf->time_int = 0;
+
+  perf->cnt = 0;
 }
 //------------------------------------------------------------------------------
 void perf_start(perf_t *perf)
 {
-  perf->time_start = PERF_GET_TICK();//htim2.Instance->CNT;
-  perf->cnt++;
-}
-//------------------------------------------------------------------------------
-void perf_end_start(perf_t *perf)
-{
-  perf_end(perf);
-  perf_start(perf);
+  uint32_t time, time_diff;
+
+  time      = PERF_GET_TICK();
+  time_diff = time - perf->time_last;
+
+  perf->time_last = time;
+  perf->time_int += time_diff;
 }
 //------------------------------------------------------------------------------
 void perf_end(perf_t *perf)
 {
-  perf->time_diff = PERF_GET_TICK() - perf->time_start;
+  uint32_t time, time_diff;
 
-  if(perf->time_diff < perf->time_min) perf->time_min = perf->time_diff;
-  if(perf->time_diff > perf->time_max) perf->time_max = perf->time_diff;
+  time      = PERF_GET_TICK();
+  time_diff = time - perf->time_last;
 
-  perf->time_avg += (float)perf->time_diff * 0.001 - perf->time_avg * 0.001;
+  if(time_diff < perf->time_min) perf->time_min = time_diff;
+  if(time_diff > perf->time_max) perf->time_max = time_diff;
+
+  perf->time_last = time;
+  perf->time_tot += time_diff;
+  perf->time_int += time_diff;
+
+  perf->cnt++;
 }
 //------------------------------------------------------------------------------
-void perf_int_proc(perf_t *perf)
+void perf_eval(perf_t *perf, perf_stat_t *stat)
 {
-  perf->cnt_int = perf->cnt;
+  float tick;
+
+  // Statistics
+  tick = 1.0 / (float)PERF_SRC_CLK;
+
+  stat->time_min = (float)perf->time_min * tick;
+  stat->time_max = (float)perf->time_max * tick;
+  stat->time_tot = (float)perf->time_tot * tick;
+  stat->time_avg = stat->time_tot / (float)perf->cnt;
+
+  stat->time_int = (float)perf->time_int * tick;
+
+  stat->loop_cnt = perf->cnt;
+
+  // Clear
+  perf->time_min = (uint32_t)-1;
+  perf->time_max = 0;
+
+  perf->time_tot = 0;
+  perf->time_int = 0;
+
   perf->cnt = 0;
-
-  perf->time_min_int = perf->time_min;
-  perf->time_min = perf->time_avg;
-
-  perf->time_max_int = perf->time_max;
-  perf->time_max = perf->time_avg;
 }
 //------------------------------------------------------------------------------
--- perf.h
+++ perf.h
@@ -17,40 +17,55 @@
 #include <string.h>
 #include <stdint.h>
 
+#include "tim.h"
+
 /* Exported types ------------------------------------------------------------*/
 /**
- * @brief  Ring buffer structure definition
+ * @brief  Performance structure definition
  */
 typedef struct
 {
-  uint32_t cnt;
-  uint32_t cnt_int;
+  uint32_t time_last;
 
-  uint32_t time_start;
-
-  uint32_t time_diff;
   uint32_t time_min;
   uint32_t time_max;
-  float time_avg;
 
-  uint32_t time_min_int;
-  uint32_t time_max_int;
+  uint64_t time_tot;
+  uint64_t time_int;
+
+  uint64_t cnt;
 
 } perf_t;
+
+/**
+ * @brief  Performance statistics structure definition
+ */
+typedef struct
+{
+  float time_min;
+  float time_max;
+  float time_avg;
+  float time_tot;
+
+  float time_int;
+
+  uint64_t loop_cnt;
+
+} perf_stat_t;
 
 /* Exported functions ------------------------------------------------------- */
 
 /* Defines -------------------------------------------------------------------*/
-#define PERF_GET_TICK() (htim2.Instance->CNT)
+#define PERF_GET_TICK() __HAL_TIM_GET_COUNTER(&htim5)
+#define PERF_SRC_CLK    84000000
 // Functions -------------------------------------------------------------------
 
 void perf_init(perf_t *perf);
 
 void perf_start(perf_t *perf);
-void perf_end_start(perf_t *perf);
 void perf_end(perf_t *perf);
 
-void perf_int_proc(perf_t *perf);
+void perf_eval(perf_t *perf, perf_stat_t *stat);
 
    //------------------------------------------------------------------------------
 #ifdef __cplusplus
